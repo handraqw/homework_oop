984e80652264e276e51dd300f032ee2ccd31e325
handraqw <boborakhimovmm@gmail.com>
2025-10-23 17:04:21 +0500

    Update praktika1.py

--- praktika1.py
+++ praktika1.py
@@ -10,10 +10,15 @@ class Employee:
     def info(self):
         return f"{self.name} {self.work_experience} {self.high_educated}"
     
+    def salary(self):
+        return 50_000 * self.work_experience * 1.1 * (int(self.high_educated) + 1)
+      
 
 
 
-# class Manager(Employee):
+class Manager(Employee):
+    def __init__(self, name, work_experience, high_educated):
+        super().__init__(name, work_experience, high_educated)
 
 # class Developer(Employee):
 
