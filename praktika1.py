d317bf9841fc77b0b8c0d9ac53c0abe956912eec
handraqw <boborakhimovmm@gmail.com>
2025-10-23 16:58:59 +0500

    Update praktika1.py

--- praktika1.py
+++ praktika1.py
@@ -2,11 +2,18 @@
 class Employee:
     category = "General"
 
+    def __init__(self, name: str, work_experience: int, high_educated: bool):
+        self.name = name
+        self.work_experience = work_experience
+        self.high_educated = high_educated  
 
-
-class Manager(Employee):
-
-class Developer(Employee):
+    def info(self):
+        return f"{self.name} {self.work_experience} {self.high_educated}"
     
 
+
+
+# class Manager(Employee):
+
+# class Developer(Employee):
 
