83a7b394f820f3f248c3293393dd5d4722836ad2
handraqw <boborakhimovmm@gmail.com>
2025-10-29 22:45:04 +0500

    add docstrings

--- praktika2.py
+++ praktika2.py
@@ -1,23 +1,45 @@
 class Vehicle:
+    """Родительский класс для транспортных средств"""
+
     def __init__(self, brand, model):
+        """
+        Инициализация транспортного средства.
+        brand: марка/производитель
+        model: модель транспортного средства
+        """
+
         self.brand = brand
         self.model = model
     
     def description(self):
+        """"Информация о транспортном средстве"""
+
         return f"Транспортное средство: {self.brand} {self.model}"
     
 class Car(Vehicle):
+    """Класс автомобиля, наследует Vehicle."""
+
     def description(self):
+        """"Информация о транспортном средстве"""
+
         return f"Автомобиль: {self.brand} {self.model} — удобен для личных поездок"
     
 
 class Bike(Vehicle):
+    """Класс велосипеда, наследует Vehicle."""
+
     def description(self):
+        """"Информация о транспортном средстве"""
+
         return f"Велосипед: {self.brand} {self.model} — мобилен и полезен"
     
 
 class Motorcycle(Vehicle):
+    """Класс мотоцикла, наследует Vehicle."""
+    
     def description(self):
+        """"Информация о транспортном средстве"""
+
         return f"Мотоцикл: {self.brand} {self.model} — быстр и манёврен"
     
 
