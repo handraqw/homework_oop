424587cc8b783b1aa812229fbad85c9b5ba5798d
handraqw <boborakhimovmm@gmail.com>
2025-10-29 20:40:58 +0500

    UPDATE PRAKTIKA.1

--- praktika1.py
+++ praktika1.py
@@ -8,3 +8,21 @@ class Employee:
     
     def __str__(self):
         return f"Сотрудник: {self.name}, зарплата: {self.calculate_salary()}"
+
+class Manager(Employee):
+    def __init__(self, name, base_salary, bonus):
+        super().__init__(name, base_salary)
+        self.bonus = bonus
+
+    def calculate_salary(self):
+
+        return self.base_salary + self.bonus
+    
+class Developer(Employee):
+    def __init__(self, name, base_salary, coefficient):
+        super().__init__(name, base_salary)
+        self.coefficient = coefficient
+
+    def calculate_salary(self):
+        return self.base_salary * self.coefficient
+
