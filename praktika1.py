045c52b386d0d4bef70001cc8b7e257e583371f8
handraqw <boborakhimovmm@gmail.com>
2025-10-29 22:42:17 +0500

    add docstring

--- praktika1.py
+++ praktika1.py
@@ -1,30 +1,45 @@
 class Employee:
+    """Родительский класс для сотрудника с именем и базовой зарплатой."""
+
     def __init__(self, name, base_salary):
         self.name = name
         self.base_salary = base_salary
 
     def calculate_salary(self):
+        """Возвращает базовую зарплату сотрудника."""
+
         return self.base_salary
     
     def __str__(self):
+        """Информация о сотруднике"""
+
         return f"Сотрудник: {self.name}, зарплата: {self.calculate_salary()}"
 
 class Manager(Employee):
+    """Класс менеджера, наследует Employee и добавляет бонус к зарплате."""
+
     def __init__(self, name, base_salary, bonus):
         super().__init__(name, base_salary)
         self.bonus = bonus
 
     def calculate_salary(self):
+        """Возвращает зарплату менеджера с учётом бонуса."""
 
         return self.base_salary + self.bonus
     
 class Developer(Employee):
+    """Класс разработчика, наследует Employee и использует коэффициент для расчёта зарплаты."""
+
     def __init__(self, name, base_salary, coefficient):
         super().__init__(name, base_salary)
         self.coefficient = coefficient
 
     def calculate_salary(self):
+        """Возвращает зарплату менеджера с учётом бонуса."""
+
         return int(self.base_salary * self.coefficient)
+    
+    
 
 staff_manager = Manager('Андрей', 100_000, 20_000)
 staff_developer = Developer('Никита', 100_000, 1.3)
